813035b7941ed160118bdceac8d29f3f5bf6cb77
WarpedTime <silverstorm79@gmail.com>
2017-03-08 18:26:18 -0500

    skeleton of node and gamemanager in a new scene

---
+++ AIEnvironment/Assets/Scripts/nodeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nodeScript : MonoBehaviour {
+
+	[SerializeField]
+	List<GameObject> neighbors;
+	[SerializeField]
+	int weight;
+	bool visited;
+	GameObject previousNode;
+	float lowestTotalCost = Mathf.Infinity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
