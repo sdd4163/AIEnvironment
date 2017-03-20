94dcce96fd20e533b3126d282ce832a583604224
WarpedTime <silverstorm79@gmail.com>
2017-03-19 22:37:29 -0400

    started on the path follower

---
+++ AIEnvironment/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour {
+
+	[SerializeField]
+	bool moving = true;
+
+	[SerializeField]
+	bool arrived = false;
+
+	[SerializeField]
+	float speed;
+
+	[SerializeField]
+	GameObject currentTarget;
+	int index = 0;
+
+	[SerializeField]
+	List<GameObject> targets;
+
+	Rigidbody RB;
+
+	// Use this for initialization
+	void Start () {
+		currentTarget = targets [index];
+		RB = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (moving) {
+
+			Vector3 tempDir = currentTarget.transform.position - gameObject.transform.position;
+			float dist = tempDir.magnitude;
+			tempDir.Normalize ();
+
+			if(dist< 10){
+				Debug.Log ("arrived!");
+				index++ ; index = index % targets.Count;
+				currentTarget = targets [index];
+			}
+
+			RB.AddForce(tempDir * speed);
+
+			transform.right = tempDir.normalized;
+		}
+
+	}
+}
