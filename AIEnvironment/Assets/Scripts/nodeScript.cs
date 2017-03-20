9ffed6707cbcec5ba3f0ec327d6023945fcbbaee
Charles Williams <crw4147@rit.edu>
2017-03-19 21:11:34 -0400

    Node tag added

--- AIEnvironment/Assets/Scripts/nodeScript.cs
+++ AIEnvironment/Assets/Scripts/nodeScript.cs
@@ -2,25 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class nodeScript : MonoBehaviour {
 
-	[SerializeField]
-	List<GameObject> neighbors;
+public class nodeScript : MonoBehaviour
+{
+    Gizmo gizmo;
+    List<GameObject> neighbors;
+    public bool visited = false;
+    GameObject previousNode;
+    float lowestTotalCost = Mathf.Infinity;
 
-	[SerializeField]
-	int weight;
+    // any visible nodes within this radius will be counting 
+    private float detectRadius;
 
-	bool visited;
-	GameObject previousNode;
-	float lowestTotalCost = Mathf.Infinity;
+    // Use this for initialization
+    void Start()
+    {
+        gizmo = this.GetComponent<Gizmo>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // get detectRadius from this nodes gizmo script
+        detectRadius = gizmo.gizmoSize;
+
+        // calculate all node neighbors
+        GameObject[] graph = GameObject.FindGameObjectsWithTag("Node");
+
+        for (int i = 0; i < graph.Length; i++)
+        {
+            if((graph[i].transform.position - this.transform.position).magnitude <= detectRadius)
+            {
+                neighbors.Add(graph[i]);
+            }
+        }
+
+        // drop the node onto the terrain
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
+
